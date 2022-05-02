41aaf088253d62bc84d44440556f15d433f00067
FlutLab.io <>
2022-05-02 18:22:32 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:gonzalez/gridviewarticulos.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(UberApp());
 }
 
-class MyApp extends StatelessWidget {
+class UberApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter App Learning',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
-        home: MyHomePage());
+        home: PaginaInicial());
   }
 }
